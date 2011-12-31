8abe49ec168af1e6ad1e49397dde13a654d983a7
Rob Bastian <rob.bastian@tippr.com>
2011-12-31 12:29:54 -0600

    Adding support for returning a JSON string.  Nice.

--- game/views.py
+++ game/views.py
@@ -1,6 +1,7 @@
 __author__ = 'rbastian'
 
 from game import app
+from json import dumps
 
 from flask import render_template, flash, url_for, redirect
 
@@ -8,4 +9,6 @@ from google.appengine.api import users
 
 @app.route('/')
 def hello_world():
-    return 'hello world'
+
+    json_str = dumps({ 'name':'Rob', 'age':42})
+    return json_str
